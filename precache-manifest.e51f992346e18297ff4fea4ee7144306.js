c0a31f3061a1061294402f37ebb420a3b3f06fec
laraveljon <jar_alanis@outlook.com>
2020-07-05 03:24:02 -0500

    Updates

---
+++ precache-manifest.e51f992346e18297ff4fea4ee7144306.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c49707848f49c256c010563edfb5aa3",
+    "url": "/jar_rick_morty/index.html"
+  },
+  {
+    "revision": "fec4bc00ed14191280c1",
+    "url": "/jar_rick_morty/static/css/main.8ba7a252.chunk.css"
+  },
+  {
+    "revision": "85f0987ee6956fed8947",
+    "url": "/jar_rick_morty/static/js/2.652ec3b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jar_rick_morty/static/js/2.652ec3b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec4bc00ed14191280c1",
+    "url": "/jar_rick_morty/static/js/main.20a122d0.chunk.js"
+  },
+  {
+    "revision": "017a5a6786113ed74e04",
+    "url": "/jar_rick_morty/static/js/runtime-main.5ea08177.js"
+  }
+]);
